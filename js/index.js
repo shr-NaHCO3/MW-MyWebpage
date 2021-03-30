9d7cc31776d88104e8a8d6a563738be7edbfde8d
shr-NaHCO3 <80567300+shr-NaHCO3@users.noreply.github.com>
2021-03-30 22:29:55 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -59,47 +59,57 @@ function login_close(){
     document.getElementById("_loginFrame").innerHTML = '<p id="_loginFrame"></p>';
     location.reload();
 }
+
 function re(){
     var reFrame = '<div id="reFrame">\
     <p>&nbsp;&nbsp;注册</p>\
     <p>&nbsp;&nbsp;用户名：<input type="text" id="re_name" /></p>\
     <p>&nbsp;&nbsp;密码： &nbsp;&nbsp;<input type="password" id="re_password" /></p>\
     <p>&nbsp;&nbsp;确认密码：<input type="password" id="re_password2" /></p>\
-    <p id="re_ts" style="color:green;">注册</p>\
+    <p id="re_ts" style="color:green;">登录</p>\
     <button id="re_ok" onClick="re_s()">确定</button>\
     <button id="re_no" onClick="re_close()">退出</button>\
     </div>';
     document.getElementById("_reFrame").innerHTML = reFrame;
 }
-function re_s(){
-    var newUserName = document.getElementById("re_name").value;
-    var newUserLock = document.getElementById("re_password").value;
-    var doubleLock  = document.getElementById("re_password2").value;
-
-    var keys = 0;
-    var isdoubleName = false;
-
-    if(newUserName == ""){
-        document.getElementById("re_ts").innerHTML = '<p id="re_ts" style="color:red;">用户名不能为空！</p>';
-        return ;
-    }
-
-    document.getElementById("re_ts").innerHTML = '<p id="re_ts" style="color:red;">加载中</p>'
-    for(var i=0;i<localStorage.length;i++){
-        keys = localStorage.key(i);
-        if(keys == newUserName){
-            isdoubleName = true;
-            break;
-        } 
-    }
-    if(isdoubleName){
-        document.getElementById("re_ts").innerHTML = '<p id="re_ts" style="color:red;">用户已存在！</p>'
-    }else{
-        console.log("y")
-    }
-}
 function re_close(){
     document.getElementById("_reFrame").innerHTML = '<p id="_reFrame"></p>';
     location.reload();
+}
+function re_s(){
+    var newUserName = document.getElementById("re_name").value;
+    var newUserLock = document.getElementById("re_password").value;
+    var newUserDoubleLock = document.getElementById("re_password2").value;
+
+    var ts = document.getElementById("re_ts");
+
+    if(newUserName == ""){
+        ts.innerHTML = '<p id="re_ts" style="color:red;">用户名不能为空！</p>';
+        return ;
+    }
+    for(var i=0;i<localStorage.length;i++){
+        if(localStorage.key(i) == newUserName){
+            ts.innerHTML = '<p id="re_ts" style="color:red;">用户已存在！</p>';
+            return ;
+        }
+    }
+    if(newUserName.slice(0,1) == "#" || newUserName.slice(-2) == "#*" ){
+        ts.innerHTML = '<p id="re_ts" style="color:red;">禁止注册一个控制单元级别的用户！</p>';
+        return ;
+    }
+    if(newUserLock == ""){
+        ts.innerHTML = '<p id="re_ts" style="color:red;">宁的密码哪去了呢？</p>';
+        return ;
+    }
+    if(newUserLock != newUserDoubleLock){
+        ts.innerHTML = '<p id="re_ts" style="color:red;">您的密码前后不一致哦！</p>';
+        return ;
+    }
+    ts.innerHTML = '<p id="re_ts" style="color:green;">正在注册...</p>';
+    var obj = {
+        "password":newUserLock
+    }
+    localStorage.setItem(newUserName,JSON.stringify(obj));
+    ts.innerHTML = '<p id="re_ts" style="color:green;">注册成功！</p>';
 }
 
